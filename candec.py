a911022b86960a21949623d5a0e2382fd3a16cea
Michal Horn <hornmich@fel.cvut.cz>
2015-04-01 00:06:15 +0200

    Rewrite the code to OOP style

--- candec.py
+++ candec.py
@@ -21,67 +21,110 @@
 
 from optparse import OptionParser
 
+class CANSignal:
+	def __init__(self, sigName='void', sigByte=0, sigBit=0, sigLen=0, sigVal=0, sigDecVal='n/a'):
+		self.sigName = sigName
+		self.sigByte = sigByte
+		self.sigBit = sigBit
+		self.sigLen = sigLen
+		self.sigVal = sigVal
+		self.sigDecVal = sigDecVal
+
+class CANDec:
+	def __init__(self):
+		self.reset()
+
+	def reset(self) :
+		self.state = 'start'
+		self.lineNum = 1
+		self.sigNames = []
+		self.signals = {}
+		self.sigRawDecoded = ''
+		
+	def printErrTok(self, token, lineNum, state):
+		print 'Unexpected token ' + token + ' found at line ' + str(lineNum) + ' in state ' + state + '\n'
+
+	def printErrParam(self, signal, numParams, lineNum, state):
+		print 'Unexpected number of parameters (' + str(numParams) + ') for signal ' + signal + ' found at line ' + str(lineNum) + ' in state ' + state + '\n'
+
+	def decode(self, canMsgVal, descriptorFileName):
+		self.reset()
+		self.descFile = open(descriptorFileName, 'r')
+
+		for line in self.descFile:
+			self.lineNum = self.lineNum + 1
+			if '%sig' in line:
+				if self.state != 'start' :
+					self.printErrTok('%sig', self.lineNum, self.state)
+					continue
+				sigParams = line.rsplit()
+				if len(sigParams) != 5:
+					self.printErrParam('%val', len(valParams), self.lineNum, self.state)
+					continue
+				tmpSigName = sigParams[1]		# Signal name
+				tmpSigByte = int(sigParams[2])	# Signal Byte
+				tmpSigBit = int(sigParams[3])	# Signal start bit
+				tmpSigLen = int(sigParams[4]);	# Signal length in bits
+				tmpValDescriptors = {}
+				self.state = 'sig'
+				
+			elif '%val' in line:
+				if self.state != 'sig' :
+					self.printErrTok('%val', self.lineNum, self.state)
+					continue
+				valParams = line.rsplit('\t')
+				if len(valParams) != 3:
+					self.printErrParam(tmpSigName, len(valParams), self.lineNum, self.state)
+					continue
+				tmpSigVal = int(valParams[1])	# Signal Value
+				tmpSigDesc = valParams[2]	# Signal value description
+				tmpValDescriptors[tmpSigVal] = tmpSigDesc
+			elif '%endsig' in line:
+				if self.state != 'sig' :
+					self.printErrTok('%endsig', self.lineNum, self.state)
+					continue
+				self.state = 'start'
+				self.sigNames.append(tmpSigName)
+				byte = (canMsgVal >> (tmpSigByte-1)*8) & 0xFF
+				bitCodeMask = 0x0
+				for i in range(tmpSigLen) :
+					bitCodeMask = bitCodeMask | 1 << i;
+				tmpSigDecVal = (byte >> tmpSigBit) & bitCodeMask;
+				if tmpSigDecVal in tmpValDescriptors:
+					self.signals[tmpSigName] = CANSignal(tmpSigName, tmpSigByte, tmpSigBit, tmpSigLen, tmpSigDecVal, tmpValDescriptors[tmpSigDecVal])
+					self.sigRawDecoded = self.sigRawDecoded + (tmpSigName + '[' + str(tmpSigByte) + ', ' + str(tmpSigBit) + ', ' + str(tmpSigLen) + '] = ' + ' (' + str(tmpSigDecVal) + ')' + tmpValDescriptors[tmpSigDecVal])
+				else:
+					self.signals[tmpSigName] = CANSignal(tmpSigName, tmpSigByte, tmpSigBit, tmpSigLen, tmpSigDecVal, 'unknown')
+					self.sigRawDecoded = self.sigRawDecoded + (tmpSigName + '[' + str(tmpSigByte) + ', ' + str(tmpSigBit) + ', ' + str(tmpSigLen) + '] = ' + ' (' + str(tmpSigDecVal) + ') unknown\n')
+	
+	def getSignalByte(self, sigName):
+		return self.signals[sigName].sigByte
+		
+	def getSignalStartBit(self, sigName):
+		return self.signals[sigName].sigBit
+		
+	def getSignalLength(self, sigName):
+		return self.signals[sigName].sigLen
+	
+	def getSignalValue(self, sigName):
+		return self.signals[sigName].sigVal
+
+	def getSignalDecoded(self, sigName):
+		return self.signals[sigName].sigDecVal
+
+	def getSignalsNames(self):
+		return self.sigNames
+		
+	def getSignalsRawDecoded(self):
+		return self.sigRawDecoded
+	
 parser = OptionParser()
-
 (options, args) = parser.parse_args()
-
 if len(args) != 2:
 	exit(1)
-
 canMsgVal = int(args[0], 16)
 descFileName = args[1]
 
-file = open(descFileName, 'r')
-
-state = 'start'
-lineNum = 0
-
-for line in file:
-	if '%sig' in line:
-		if state != 'start' :
-			print 'Unexpected token %sig found at line ' + str(lineNum) + 'in state ' + state
-			continue
-		params = line.rsplit()
-		if len(params) != 5:
-			continue
-		sigName = params[1]
-		startByte = int(params[2])
-		startBit = int(params[3])
-		segLen = int(params[4]);
-		valDescriptor = {}
-		state = 'sig'
-		
-	elif '%val' in line:
-		if state != 'sig' :
-			print 'Unexpected token %val found at line ' + str(lineNum) + 'in state ' + state
-			continue
-		params = line.rsplit('\t')
-		if len(params) != 3:
-			continue
-		descriptor = params[2]
-		value = int(params[1])
-		#print 'Descriptor: ' + descriptor[2]
-		valDescriptor[value] = descriptor
-	elif '%endsig' in line:
-		if state != 'sig' :
-			print 'Unexpected token %endsig found at line ' + str(lineNum) + 'in state ' + state
-			continue
-		state = 'start'
-		
-		byte = (canMsgVal >> (startByte-1)*8) & 0xFF
-		#print 'Byte: ' + str(byte)
-		bitCodeMask = 0x0
-		for i in range(segLen) :
-			bitCodeMask = bitCodeMask | 1 << i;
-		#print 'bitCodeMask: ' + str(bitCodeMask)
-		bitCode = (byte >> startBit) & bitCodeMask;
-		#print 'bitCode: ' + str(bitCode)
-		if len(valDescriptor) > 0 and bitCode >= len(valDescriptor):
-			print sigName + '[' + str(startByte) + ', ' + str(startBit) + ', ' + str(segLen) + '] = ' + ' (' + str(bitCode) + ') undefined\n' 
-		elif len(valDescriptor) == 0:
-			print sigName + '[' + str(startByte) + ', ' + str(startBit) + ', ' + str(segLen) + '] = ' + str(bitCode) + '\n' 
-		else:
-			print sigName + '[' + str(startByte) + ', ' + str(startBit) + ', ' + str(segLen) + '] = ' + ' (' + str(bitCode) + ')' + valDescriptor[bitCode] 
-print 'done'
-
-
+canDecoder = CANDec()
+canDecoder.decode(canMsgVal, descFileName)
+print canDecoder.getSignalsRawDecoded()
